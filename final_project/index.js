06f3b5ffdaa93f3ff2c478758f28ccccb2068103
madhav2606 <128503383+madhav2606@users.noreply.github.com>
2024-06-26 17:58:27 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
 if(req.session.authorization) { //get the authorization object stored in the session
     token = req.session.authorization['accessToken']; //retrieve the token from authorization object
     jwt.verify(token, "access",(err,user)=>{ //Use JWT to verify token
